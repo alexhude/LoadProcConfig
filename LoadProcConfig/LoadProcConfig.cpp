468c9927fea74900dee41ba62170471965156752
Alexander Hude <alexhude@users.noreply.github.com>
2017-05-19 00:01:50 +1000

    LoadProcConfig initial commit: - Add source code project - Add plugin binaries - Add readme

---
+++ LoadProcConfig/LoadProcConfig.cpp
@@ -0,0 +1,179 @@
+//
+// LoadProcConfig.cpp
+// Load Processor Config
+//
+// Created by Alexander Hude on 31/03/16.
+// Copyright (c) 2017 Fried Apple Team. All rights reserved.
+//
+
+#include <ida.hpp>
+#include <idp.hpp>
+#include <loader.hpp>
+#include <auto.hpp>
+
+// The netnode helper.
+// Using this node we will save current configuration information in the IDA database.
+static netnode helper;
+
+char device[MAXSTR] = "";
+char cfgfile[QMAXFILE];
+
+//--------------------------------------------------------------------------
+
+#include <name.hpp>
+#include <offset.hpp>
+#include <diskio.hpp>
+
+static size_t numports = 0;
+static ioport_t *ports = NULL;
+
+#define NO_GET_CFG_PATH
+
+inline void get_cfg_filename(char *buf, size_t bufsize, bool user = false)
+{
+	buf[0] = 0;
+	
+	if (user == true)
+	{
+		int back_cnt = 0;
+		size_t base_offset = 0;
+
+		char* filename = askfile2_c(false, nullptr, "*.cfg", "Load Processor Configuration");
+		
+		// choose_ioport_device() only supports path relative to 'cfg' folder in IDA
+		// therefore we need to generate it from our destination path
+		
+		// get 'cfg' path
+		char cfg_path[QMAXFILE] = {0};
+		qstrncpy(cfg_path, idadir(CFG_SUBDIR), QMAXFILE);
+		
+		// find common base and generate path to it from the source
+		while (qstrstr(filename, cfg_path) == nullptr)
+		{
+			char* slash_pos = qstrrchr(cfg_path, '/');
+			if (slash_pos == nullptr)
+				break;
+			
+			qstrncat(buf, "../", bufsize);
+			slash_pos[0] = 0;
+			back_cnt++;
+		}
+		base_offset = strlen(cfg_path);
+		
+		// create relative path to destination
+		qstrncat(buf, filename + base_offset + 1, bufsize); // exclude left '/' from path
+	}
+	else
+	{
+		qstrncpy(buf, cfgfile, bufsize);
+	}
+}
+
+// include IO common routines (such as set_device_name, apply_config_file, etc..)
+#include "../module/iocommon.cpp"
+
+//--------------------------------------------------------------------------
+void run(int)
+{
+	get_cfg_filename(cfgfile, QMAXFILE, true);
+
+	if (cfgfile[0] == 0)
+		return;
+	
+	msg("ProcConf: loading config \"%s\"...\n", cfgfile);
+	
+	if ( choose_ioport_device(cfgfile, device, sizeof(device), NULL) )
+	{
+		msg("ProcConf: ... done\n");
+		if (qstrcmp(device, "NONE") != 0)
+		{
+			msg("ProcConf: device chosen \"%s\"\n", device);
+			
+			int resp_info = IORESP_ALL;
+			display_infotype_dialog(IORESP_ALL, &resp_info, cfgfile);
+			
+			set_device_name(device, resp_info);
+			noUsed(0, BADADDR); // reanalyze program
+		}
+	}
+	else
+	{
+		msg("ProcConf: ... failed\n");
+	}
+}
+
+//--------------------------------------------------------------------------
+
+const ioport_t *find_sym(ea_t address)
+{
+	return find_ioport(ports, numports, address);
+}
+
+int idaapi hook(void* user_data, int notification_code, va_list va)
+{
+	switch (notification_code) {
+		case processor_t::custom_outop:
+		{
+			op_t* op = va_arg(va, op_t *);
+			if (op->type == o_imm)
+			{
+				const ioport_t * port = find_sym(op->value);
+				if ( port != NULL )
+				{
+					out_line(port->name, COLOR_IMPNAME);
+					return 2;
+				}
+			}
+			break;
+		}
+		default:
+			break;
+	}
+	
+	return 0;
+}
+
+//--------------------------------------------------------------------------
+int init(void)
+{
+  hook_to_notification_point(HT_IDP, hook, NULL);
+  return PLUGIN_KEEP;
+}
+
+//--------------------------------------------------------------------------
+void term(void)
+{
+  unhook_from_notification_point(HT_IDP, hook, NULL);
+}
+
+//--------------------------------------------------------------------------
+char help[] = "Load Processor Config";
+char comment[] = "This module allows to load processor configuration files";
+char wanted_name[] = "Load Processor Config";
+char wanted_hotkey[] = "";
+
+
+//--------------------------------------------------------------------------
+//
+//      PLUGIN DESCRIPTION BLOCK
+//
+//--------------------------------------------------------------------------
+plugin_t PLUGIN =
+{
+  IDP_INTERFACE_VERSION,
+  PLUGIN_PROC,          // plugin flags
+  init,                 // initialize
+
+  term,                 // terminate. this pointer may be NULL.
+
+  run,                  // invoke plugin
+
+  comment,              // long comment about the plugin
+                        // it could appear in the status line
+                        // or as a hint
+
+  help,                 // multiline help about the plugin
+
+  wanted_name,          // the preferred short name of the plugin
+  wanted_hotkey         // the preferred hotkey to run the plugin
+};
--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# LoadProcConfig
-IDA plugin to load processor configuration files.
+## Overview
+**LoadProcConfig** is an IDA Plugin to load processor configuration files.
+
+With just a couple of clicks configuration files can turn this:
+![](./Resources/screenshots/lpc_before.png)
+Into this, adding memory regions, entries and registers/ports:
+![](./Resources/screenshots/lpc_after.png)
+By default IDA is shipped with configuration files for many processors in **IDA/cfg** folder. These configuration files are loaded automatically when corresponding processor module is loaded.  
+
+However, this approach has some disadvantages:  
+1. You have to keep your own configuration files inside IDA  
+2. There is no easy way to load and apply configuration file on existing database  
+3. Most important, ARM processor module doesn't have configuration file  
+
+Current plugin was implemented to address all these issues.
+
+## Installation
+
+Just copy **LoadProcConfig.pmc** and **LoadProcConfig.pmc64** to IDA plugins folder.
+
+## Usage
+
+From **Edit / Plugins** menu choose **Load Processor Config**.  
+Then open configuration file using the file dialog.
+IDA will parse this file and display all available devices.  
+Pick your device and press **OK**.  
+![](./Resources/screenshots/lpc_device.png)
+Choose what do you want to load from this configuration file.
+![](./Resources/screenshots/lpc_info.png)
+Plugin will reanalyse entire database using device description and create additional memory regions.
+
+## Configuration files
+
+There are many configuration files in **IDA/cfg** folder. While the format has no proper documentation, we can get some ideas by looking into **idasdk/module/iocommon.cpp**
+
+Every configuration includes following parts:
+
+```
+; Default device in current configuration file
+.default MYDEVICE
+
+; Device description
+.MYDEVICE
+
+; RAM/FLASH specification
+RAM=512
+ROM=8192
+EEPROM=512
+
+; Memory Map
+area DATA I_SRAM    0x00060:0x00260  Internal SRAM
+area DATA E_SRAM    0x00260:0x10000  External SRAM
+
+; Interrupts and reset vector assignments 
+entry RESET         0x0000   External Reset, Power-on Reset and Watchdog Reset
+entry UART_RX       0x0009   UART, Rx Complete
+
+; Registers (with bit fields)
+USR                 0x000B   UART Status Register
+USR.RXC              7   UART Receive Complete
+USR.TXC              6   UART Transmit Complete
+USR.UDRE             5   UART Data Register Empty
+USR.FE               4   Framing Error
+USR.OR               3   Overrun
+```
+
+You can find example for STM32F40x in **Configurations** folder
